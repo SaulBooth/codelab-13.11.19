baa1d5fc87c59d848a38694c84d92382c0226a1d
SaulBooth <SaulBooth@users.github.com>
2019-11-13 11:47:32 +0000

    Iterate through a word exercise

---
+++ Iterate through a word exercise/Iterate through a word exercise.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i <= 2; i++) {
+		string myword = "Joe";
+		cout << myword.at(i) << endl;
+	}
+	cout << endl;
+
+	return 0;
+}
