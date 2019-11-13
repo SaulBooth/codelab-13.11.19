fb2a5d10392d8c116847b485c71131433242642c
SaulBooth <SaulBooth@users.github.com>
2019-11-13 11:25:29 +0000

    some counting exercise

---
+++ Some counting exercise/Some counting exercise.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//1
+	for (int i = 0; i <= 50; i++) {
+		cout << i << endl;
+	}
+	cout << endl;
+
+	//2
+	for (int i = 50; i >= 0; i--) {
+		cout << i << endl;
+	}
+	cout << endl;
+
+	//3
+	for (int i = 30; i <= 50; i++) {
+		cout << i << endl;
+	}
+	cout << endl;
+
+	//4
+	for (int i = 50; i >= 10; i--) {
+		cout << i << endl;
+	}
+	cout << endl;
+
+	//5
+	for (int i = 100; i <= 200; i=i+5) {
+		cout << i << endl;
+	}
+	cout << endl;
+
+	return 0;
+}
